c8d5a6aabb6d79b195ca9157f00deb8f199780df
friedebold <daniel@friedebold.net>
2024-09-15 22:56:59 +0200

    updated battery pins

--- src/battery.cpp
+++ src/battery.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 #include <battery.h>
 
-const int VBAT1_PIN = A7;
-const int VBAT2_PIN = A4;
-const int VBAT3_PIN = A5;
+const int VBAT1_PIN = 39;
+const int VBAT2_PIN = 36;
+const int VBAT3_PIN = 34;
 const float VCC = 3.31;
 
 void setup_battery()
@@ -17,7 +17,7 @@ void read_battery()
 {
     int vBat1Input = analogRead(VBAT1_PIN);
     float vBat1Voltage = vBat1Input * (VCC / 4095.0);
-    float vBat1 = vBat1Voltage * 1.368627451;
+    float vBat1 = vBat1Voltage; //* 1.368627451;
 
     int vBat2Input = analogRead(VBAT2_PIN);
     float vBat2Voltage = vBat2Input * (VCC / 4095.0);
